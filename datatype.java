f3e442eb45a9e793b56019a39f0c8d0239316025
Ayushi shukla <ayushishukla66237@gmail.com>
2025-04-08 11:19:27 +0530

    hello

---
+++ datatype.java
@@ -0,0 +1,27 @@
+public class DataTypesExample {
+    public static void main(String[] args) {
+        // Integer type (whole numbers)
+        int number = 100;
+        
+        // Floating-point types (decimal numbers)
+        float pi = 3.14f;  // 'f' is used to indicate a float
+        double e = 2.71828; // double precision
+        
+        // Character type (single character)
+        char letter = 'A';
+        
+        // Boolean type (true or false)
+        boolean isJavaFun = true;
+        
+        // String type (a sequence of characters)
+        String greeting = "Hello, Java!";
+        
+        // Printing the values of different data types
+        System.out.println("Integer: " + number);
+        System.out.println("Float: " + pi);
+        System.out.println("Double: " + e);
+        System.out.println("Character: " + letter);
+        System.out.println("Boolean: " + isJavaFun);
+        System.out.println("String: " + greeting);
+    }
+}
