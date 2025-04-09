a593e228109ebfe2111bb31c23ca53c5ff163707
Ayushi shukla <ayushishukla66237@gmail.com>
2025-04-09 11:24:18 +0530

    java

---
+++ HelloJava.java
@@ -0,0 +1,11 @@
+public class HelloJava {
+    public static void main(String[] var0) {
+        byte var1 = 10;
+        byte var2 = 12;
+        System.out.println("Addition: " + (var1 + var2));
+        System.out.println("Substraction: " + (var1 - var2));
+        System.out.println("multiplication: " + var1 * var2);
+        System.out.println("division: " + var1 / var2);
+        System.out.println("modulus: " + var1 % var2);
+     }
+}
---
+++ ifelse.java
@@ -0,0 +1,13 @@
+public class ifelse {
+    public static void main(String[] var0) {
+        byte var1 = 6;
+        if (var1 > 0) {
+           System.out.println("num is positive");
+        } else if (var1 == 0) {
+           System.out.println("num is zero");
+        } else {
+           System.out.println("num is negative");
+        }
+  
+     }
+}
