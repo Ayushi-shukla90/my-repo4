fcc3232c03267cdea250fcb752eef8b6e7540782
Ayushi shukla <ayushishukla66237@gmail.com>
2025-04-09 10:52:49 +0530

    java

---
+++ ifelse.java
@@ -0,0 +1,14 @@
+public class ifelse {
+    public static void main(String[] args){
+        int num = 6;
+        if(num > 0){
+            System.out.println("num is positive");
+        }
+        else if(num == 0){
+            System.out.println("num is zero");
+        }
+        else{
+            System.out.println("num is negative");
+        }
+    }
+}
