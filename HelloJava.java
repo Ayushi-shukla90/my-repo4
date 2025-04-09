6ca1bfbadd44307f919cc4ff22bd9a00c2a5599b
Ayushi shukla <ayushishukla66237@gmail.com>
2025-04-09 20:02:54 +0530

    java

--- HelloJava.java
+++ HelloJava.java
@@ -1,5 +1,5 @@
 public class HelloJava {
-    public static void main(String[] var0) {
+    public static void main(String[] args) {
         byte var1 = 10;
         byte var2 = 12;
         System.out.println("Addition: " + (var1 + var2));
--- evenodd.java
+++ evenodd.java
@@ -1,11 +1,10 @@
 public class evenodd {
     public static void main(String[] args) {
         int a = 6;
-   if(a % 2 == 0){
-    System.out.println("Even");
-   }
-   else{
-    System.out.println("Odd");
-   }
+        if (a % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
     }
 }
