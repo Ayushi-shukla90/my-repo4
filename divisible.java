c19893a11d93ae290f25bee5be7a53d7df6ca2cf
Ayushi shukla <ayushishukla66237@gmail.com>
2025-04-10 17:32:46 +0530

    java

---
+++ divisible.java
@@ -0,0 +1,11 @@
+public class divisible {
+    public static void main(String[] args) {
+    int a = 50;
+    if (a % 5 == 0 && a % 11 == 0) { 
+        System.out.println("Divisible by 5 & 11");
+    }
+    else{
+        System.out.println("Not divisible");
+    }
+    }
+}
