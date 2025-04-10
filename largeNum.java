5095d40507ee9d8de8f32c75edcbaa2673cfa671
Ayushi shukla <ayushishukla66237@gmail.com>
2025-04-10 17:15:26 +0530

    java

---
+++ largeNum.java
@@ -0,0 +1,11 @@
+public class largeNum {
+    public static void main(String[] args) {
+        int a=87,b=90;
+        if(a>b){
+            System.out.println("a is greater than b");
+        }
+        else{
+            System.out.println("b is greater than a");
+        }
+    }
+}
