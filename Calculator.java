eb10bb08784cf9789b085b48aa5f86e049568601
Ayushi shukla <ayushishukla66237@gmail.com>
2025-04-11 18:51:04 +0530

    java

--- Calculator.java
+++ Calculator.java
@@ -2,32 +2,45 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
- Scanner in = new  Scanner(System.in);
-System.out.println("Enter 1st num: ");
-int a = in.nextInt();
-System.out.println("Enter 2nd num: ");
-int b = in.nextInt();
-System.out.println("Enter command: ");
-char c = in.next(.charAt(0));
-switch(c){
-    case '+':
-    System.out.println(a+b);
-    break;
-    case '-':
-    System.out.println(a-b);
-    break;
-    case '*':
-    System.out.println(a*b);
-    break;
-    case '/':
-    System.out.println(a/b);
-    break;
-    case '%':
-    System.out.println(a%b);
-    break;
-    default:
-    System.out.println("invalid");
+        Scanner in = new Scanner(System.in);
 
-}
+        System.out.println("Enter 1st num: ");
+        int a = in.nextInt();
+
+        System.out.println("Enter 2nd num: ");
+        int b = in.nextInt();
+
+        System.out.println("Enter command (+, -, *, /, %): ");
+        char c = in.next().charAt(0);
+
+        switch(c){
+            case '+':
+                System.out.println(a + b);
+                break;
+            case '-':
+                System.out.println(a - b);
+                break;
+            case '*':
+                System.out.println(a * b);
+                break;
+            case '/':
+                if (b != 0) {
+                    System.out.println(a / b);
+                } else {
+                    System.out.println("Cannot divide by zero.");
+                }
+                break;
+            case '%':
+                if (b != 0) {
+                    System.out.println(a % b);
+                } else {
+                    System.out.println("Cannot modulo by zero.");
+                }
+                break;
+            default:
+                System.out.println("Invalid command");
+        }
+
+        in.close(); 
     }
 }
--- days.java
+++ days.java
@@ -2,38 +2,36 @@ import java.util.Scanner;
 
 public class days {
     public static void main(String[] args) {
- Scanner x = new  Scanner(System.in);
- 
- int a = x. nextInt();
- System.out.println("Enter day: ");
- switch(a) {
-    case 1 :
-    System.out.println("Monday");
-    break;
-    case 2 :
-    System.out.println("Tuesday");
-    break;
-    case 3 :
-    System.out.println("Wednesday");
-    break;
-    case 4 :
-    System.out.println("Thursday");
-    break;
-    case 5 :
-    System.out.println("Friday");
-    break;
-    case 6 :
-    System.out.println("Saturday");
-    break;
-    case 7 :
-    System.out.println("Sunday");
-    break;
+        Scanner x = new Scanner(System.in);
 
-    default :
-    System.out.println("No day");
-    
- }
- }
- }
- 
- 
+        System.out.println("Enter day: "); 
+        int a = x.nextInt();
+
+        switch(a) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("No day");
+        }
+        x.close();
+    }
+}
