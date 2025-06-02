0f4e44fad2f488220af485b2ebda944490544d37
Ayushi-shukla90 <ayushishukla66237@gmail.com>
2025-06-02 10:42:44 +0530

    java

---
+++ list.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Student implements Comparable<Student> {
+    int age;
+    String name;
+    public Student(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+    public String toString() {
+        return age + " " + name;
+    }
+    public int compareTo(Student s) {
+        return this.age - s.age; 
+    }
+}
+
+class list {
+    public static void main(String[] args) {
+        ArrayList<Student> list = new ArrayList<>();
+        list.add(new Student(12, "rahul"));
+        list.add(new Student(11, "rohit"));
+        list.add(new Student(2, "mohit"));
+        Collections.sort(list);
+        for (Student s : list) {
+            System.out.println(s);
+        }
+    }
+}
